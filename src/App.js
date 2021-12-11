1cc35a92bab8c751d9626c2980b319c8c9142ce2
Leo Mart <mistikleo7@gmail.com>
2021-12-11 15:22:05 +0200

    lesson 39

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = (props) => {
         <Navbar />
         <div className='wrapper-content'>
           <Routes>
-            <Route path='/profile' element={<Profile state={props.state.profilePage} addPost={props.addPost} updateNewPostText={props.updateNewPostText} />} />
+            <Route path='/profile' element={<Profile state={props.state.profilePage} dispatch={props.dispatch} />} />
             <Route path='/messages/*' element={<Messages state={props.state.messagesPage} />}  />
             <Route path='/friends' element={<Friends />} />
             <Route path='/music' element={<Music />} />
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,49 +1,58 @@
-let rerenderEntireTree = () =>{
-
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'How are you?', likes: '5', name: 'Leo'},
-            { id: 2, message: 'Hi, I`m Olly', likes: '7', name: 'Olly'}
-        ],
-        newPostText: ''
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'How are you?', likes: '5', name: 'Leo' },
+                { id: 2, message: 'Hi, I`m Olly', likes: '7', name: 'Olly' }
+            ],
+            newPostText: ''
+        },
+        messagesPage: {
+            dialogs: [
+                { id: 1, name: 'Leo' },
+                { id: 2, name: 'Maria' },
+                { id: 3, name: 'Dan' }
+            ],
+            messages: [
+                { id: 1, message: 'hello' },
+                { id: 2, message: 'how r u?' },
+                { id: 3, message: 'im fine' }
+            ]
+        }
     },
-    messagesPage: {
-        dialogs: [
-            { id: 1, name: 'Leo' },
-            { id: 2, name: 'Maria' },
-            { id: 3, name: 'Dan' }
-        ],
-        messages: [
-            { id: 1, message: 'hello' },
-            { id: 2, message: 'how r u?' },
-            { id: 3, message: 'im fine' }
-        ]
+    _callSubscriber() {
+
+    },
+
+    getState() {
+        return this._state
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+
+    dispatchEvent(action) {
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likes: 0,
+                name: 'Dan'
+            }
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
     }
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likes: 0,
-        name: 'Dan'
-    }
+export const addPostActionCreator = () => ({type: 'ADD-POST'}) 
 
-    state.profilePage.posts.push (newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
+export const updateNewPostTextActionCreator = (text) => ({type: 'UPDATE-NEW-POST-TEXT', newText: text})
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-export const subscribe = (observer) =>{
-    rerenderEntireTree = observer;
-}
 
-export default state;
+export default store;
